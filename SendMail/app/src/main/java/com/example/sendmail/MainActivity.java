a6c23ea3b7ed6c6833414f9efeed99121f23ddab
ry.morikage <doyourwish.soft.dev@gmail.com>
2023-05-06 21:33:18 +0900

    GoogleAPIを利用したメール送信を試行中（未完成） 実行時に次のエラーが出る java.io.IOException: The Application Default Credentials are not available.

--- SendMail/app/src/main/java/com/example/sendmail/MainActivity.java
+++ SendMail/app/src/main/java/com/example/sendmail/MainActivity.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import javax.mail.MessagingException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,73 +22,17 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                asyncTask a=new asyncTask();
-                //a.execute("kaki37yo","rrlrcvoeciavicke","テストタイトル","送信完了\n本文をここに記述する") ;
-                a.execute("donamon0","Moriryo1996","テストタイトル","送信完了\n本文をここに記述する") ;
+                String email_address = "kaki37yo@gmail.com";
+                try {
+                    SendMessage.sendEmail(email_address,email_address);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } catch (MessagingException e) {
+                    throw new RuntimeException(e);
+                } catch (GeneralSecurityException e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
-    }
-
-    private class asyncTask extends android.os.AsyncTask{
-        protected String account;
-        protected String password;
-        protected String title;
-        protected String text;
-
-        @Override
-        protected Object doInBackground(Object... obj){
-            account=(String)obj[0];
-            password=(String)obj[1];
-            title=(String)obj[2];
-            text=(String)obj[3];
-
-            java.util.Properties properties = new java.util.Properties();
-            //google(cannot)
-//            properties.put("mail.smtp.host", "smtp.gmail.com");
-//            properties.put("mail.smtp.auth", "true");
-//            properties.put("mail.smtp.port", "465");
-//            properties.put("mail.smtp.socketFactory.post", "465");
-//            properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-
-            //yahoo(cannot)
-            properties.put("mail.smtp.host", "smtp.mail.yahoo.co.jp");
-            properties.put("mail.smtp.port", "587");
-            properties.put("mail.smtp.auth", "true");
-            properties.put("mail.transport.protocol", "smtp");
-            properties.put("mail.smtp.ssl.trust", "*");
-            properties.put("mail.smtp.starttls.enable", "true");
-            properties.put("mail.smtp.connectiontimeout", "10000");
-            properties.put("mail.smtp.timeout", "10000");
-
-
-            final javax.mail.Message msg = new javax.mail.internet.MimeMessage(javax.mail.Session.getDefaultInstance(properties, new javax.mail.Authenticator(){
-                @Override
-                protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-                    return new javax.mail.PasswordAuthentication(account,password);
-                }
-            }));
-
-            try {
-                msg.setFrom(new javax.mail.internet.InternetAddress(account + "@yahoo.com"));
-                //自分自身にメールを送信
-                msg.setRecipients(javax.mail.Message.RecipientType.TO, javax.mail.internet.InternetAddress.parse(account + "@yahoo.com"));
-                msg.setSubject(title);
-                msg.setText(text);
-
-                javax.mail.Transport.send(msg);
-
-            } catch (Exception e) {
-                System.out.println("[asyncTask]send error : " + e.toString());
-                return (Object)e.toString();
-            }
-
-            return (Object)"送信が完了しました";
-
-        }
-        @Override
-        protected void onPostExecute(Object obj) {
-            //画面にメッセージを表示する
-            Toast.makeText(MainActivity.this,(String)obj,Toast.LENGTH_LONG).show();
-        }
     }
 }
