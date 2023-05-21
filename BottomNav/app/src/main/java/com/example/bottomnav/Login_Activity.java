eea7a5293092371c1dc81327f372898f5a2ac456
sota hayshi <884884884hs@gmail.com>
2023-05-22 00:05:06 +0900

    カメラ、アルバムボタンにダミー機能追加

--- BottomNav/app/src/main/java/com/example/bottomnav/Login_Activity.java
+++ BottomNav/app/src/main/java/com/example/bottomnav/Login_Activity.java
@@ -3,7 +3,9 @@ package com.example.bottomnav;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,7 +14,32 @@ import android.widget.Toast;
 
 public class Login_Activity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
+        Button Button = findViewById(R.id.forget_button);
+        // lambda式
+        Button.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplication(), Login_Forget.class);
+            startActivity(intent);
+        });
+
+        Button Button2 = findViewById(R.id.register_button);
+        // lambda式
+        Button2.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplication(), Register.class);
+            startActivity(intent);
+        });
+
+        Button Button3 = findViewById(R.id.login);
+        // lambda式
+        Button3.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplication(), MainActivity.class);
+            startActivity(intent);
+        });
+    }
 }
 
 
